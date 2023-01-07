7ac172080829a265b04451a026cc4fb1c633b2fa
Hemed Tov <hemedbz@gmail.com>
2023-01-07 16:10:33 +0200

    vt_analyzer almost finalized: with documentation, fixed reputation output

--- vt_analyzer.py
+++ vt_analyzer.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import exceptions
 
 
 class VTAnalyzer:
@@ -11,23 +12,36 @@ class VTAnalyzer:
                        "x-apikey": self._api_key}
 
     def get_analysis(self, url):
+        """
+        Gets analysis from api
+        """
+
         url_id = base64.urlsafe_b64encode(url.encode()).decode().strip("=")
         response = requests.get(f"https://www.virustotal.com/api/v3/urls/{url_id}", headers=self._header)
+
         if response.status_code == 200:
             return response.json()
         elif response.status_code == 404:
             return None
         else:
-            raise Exception
+            raise exceptions.ResponseError
 
     def scan_url(self, url) -> bool:
+        """
+        Asks api to create new analysis
+        """
+
         response = requests.post("https://www.virustotal.com/api/v3/urls", data=url, headers=self._header)
         if response.status_code == 200:
             return True
         else:
-            return False
+            raise exceptions.ResponseError
 
     def get_reputation_for_url(self, url: str):
+        """
+        gets reputation if analysis exists, if not scans and then retrieves it
+        """
+
         data = self.get_analysis(url)
 
         if data is None:
@@ -37,5 +51,6 @@ class VTAnalyzer:
 
         last_analysis_date = data["data"]["attributes"]["last_analysis_date"]
         last_analysis_results = data["data"]["attributes"]["last_analysis_stats"]
+        reputation = {"Last analysis date": last_analysis_date, "Last analysis results": last_analysis_results}
 
-        return last_analysis_date, last_analysis_results
+        return reputation
