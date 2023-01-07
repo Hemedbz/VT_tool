bee595ccaf0edeaf8c5c4455ffd23180ddb5071a
Hemed Tov <hemedbz@gmail.com>
2023-01-07 15:51:29 +0200

    Parser almost finalized: with documentation

--- parser.py
+++ parser.py
@@ -1,7 +1,12 @@
 import argparse
-#TODO: add comments, help explanations
 
-parser = argparse.ArgumentParser(prog="VT Tool")
+parser = argparse.ArgumentParser(prog="VT Tool", description="Enter urls, separated with comma no space, to check their safety analysis in virustotal.com. \n"
+                                                             "The program will return analysis results.\n"
+                                                             "-s or --scan | optional | will force new analysis scan."
+                                                             "-a or --apikey | should be followed by your api key."
+                                                             "-m or --maxage | oprional | allows you to determine maximum valid age for analysis, in days (enter integer). Default 180",
+                                 epilog="end of help")
+
 parser.add_argument("urls")
 parser.add_argument("-s", "--scan", action="store_true")
 parser.add_argument("-a", "--apikey", default=None)
