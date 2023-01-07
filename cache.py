2016255ebcbdf6fd2dbbb80d9db9a21b1c44bc44
Hemed Tov <hemedbz@gmail.com>
2023-01-07 15:08:27 +0200

    Cache almost finalized: with documentation and exceptions

--- cache.py
+++ cache.py
@@ -1,21 +1,29 @@
-import os
 import json
 import datetime
-
-#{
-# url : {"last analysis date" : last_analysis_date,
-#       "last analysis results" : last_analysis_results}
-# }
+from threading import Lock
+import exceptions
 
 class Cache:
 
     def __init__(self, file_name):
-        self._content = {}
         self._file_name = file_name
+        self._content = {}
+        self._lock = Lock()
 
     def __str__(self):
-        with open(f"{self._file_name}.json", "w") as fh:
-            return json.load(fh)
+        return self._content
+
+    def _load_json(self):
+        """
+        loads data from json file to instance
+        """
+        try:
+            with open(f"{self._file_name}.json", "r") as fh:
+                self._lock.acquire()
+                self._content = json.load(fh)
+                self._lock.release()
+        except:
+           exceptions.NoFile()
 
     def put(self, url, new_information):
         """
@@ -23,31 +31,41 @@ class Cache:
         :param url: str
         :param new_information: dictionary with last analysis date, last analysis results
         """
+        self._load_json()
+        self._content[url] = new_information
 
-        with open(f"{self._file_name}.json", "w") as fh:
-            self._content = json.load(fh)
-            self._content[url] = new_information
-            self._content.json.dump(fh)
+        try:
+            with open(f"{self._file_name}.json", "w") as fh:
+                self._lock.acquire()
+                json.dump(self._content, fh)
+                self._lock.release()
+        except:
+          exceptions.FailedToSave()
 
     def get_info(self, url: str, maxage: int):
+        """gets information about url from cache file, if the data is still valid
+        :param url:str
+        :param maxage: maximum acceptable data entry age, in days (default = 180)
+        :return dict
+        """
 
-        with open(f"{self._file_name}.json", "w") as fh:
-            self._content = json.load(fh)
+        self._load_json()
 
-        #check if url in file
+        # check if information in cache
         if url not in self._content:
             return None
 
-        #check if analysis expired
+        # check if information still valid
         if datetime.datetime.now() - datetime.datetime.fromtimestamp(self._content[url]["last analysis date"], tz=datetime.timezone.utc) >= maxage:
+            self._lock.acquire()
             del self._content[url]
-        #TODO: how to save?
-            return None
+            try:
+                with open(f"{file_name}.json", "w") as fh:
+                    json.dump(self._content, fh)
+                self._lock.release()
+                return None
+            except:
+                exceptions.FailedToSave()
 
         #give information
-        last_analysis_date = datetime.datetime(self._content[url]["last analysis date"])
-        last_analysis_results = self._content[url]["last analysis results"]
-        return last_analysis_date, last_analysis_results
-
-        with open(f"{file_name}.json", "w") as fh:
-            json.dump(self._content, fh)
+        return self._content[url]
